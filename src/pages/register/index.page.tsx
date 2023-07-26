2f13f231fdbd0caf110e50185d95e0a20c78a7c5
Bruno <brunodaniel.elias@gmail.com>
2023-07-26 16:29:22 -0300

    finish register at prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,28 +1,30 @@
-import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
-import { Container, Form, FormError, Header } from "./styles";
-import { ArrowRight } from "phosphor-react";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react"
+import { Container, Form, FormError, Header } from "./styles"
+import { ArrowRight } from "phosphor-react"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { api } from "@/lib/axios"
+import { AxiosError } from "axios"
 
 const registerFormSchema = z.object({
     username: z
         .string()
         .min(3, {
-            message: 'O nome de usuário deve ter pelo menos 3 caracteres',
+            message: "O nome de usuário deve ter pelo menos 3 caracteres",
         })
         .max(20, {
-            message: 'O nome de usuário deve ter no máximo 20 caracteres',
+            message: "O nome de usuário deve ter no máximo 20 caracteres",
         })
         .regex(/^[a-z0-9//-]+$/i, {
-            message: 'O nome de usuário não pode conter caracteres especiais',
+            message: "O nome de usuário não pode conter caracteres especiais",
         })
         .transform((username) => username.toLowerCase()),
     name: z.string().min(3, {
-        message: 'O nome de usuário deve ter pelo menos 3 caracteres',
-    })
+        message: "O nome de usuário deve ter pelo menos 3 caracteres",
+    }),
 })
 
 type RegisterFormData = z.infer<typeof registerFormSchema>
@@ -39,39 +41,63 @@ export default function Register() {
 
     const router = useRouter()
 
-    async function handleRegister(data: RegisterFormData) { }
+    async function handleRegister(data: RegisterFormData) {
+        try {
+            await api.post("/users", { name: data.name, username: data.username })
+        } catch (error) {
+            if (error instanceof AxiosError && error.response?.data.message) {
+                alert(error.response.data.message)
+                return
+            }
+
+            console.error(error)
+        }
+    }
 
     useEffect(() => {
         if (router.query.username) {
-            setValue('username', String(router.query.username))
+            setValue("username", String(router.query.username))
         }
     }, [router.query?.username, setValue])
 
     return (
         <Container>
             <Header>
-                <Heading as='strong'>Bem-vindo ao Ignite Call!</Heading>
-                <Text>Precisamos de algumas informações para criar seu perfil! Ah, você pode editar essas informações depois.</Text>
+                <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+                <Text>
+                    Precisamos de algumas informações para criar seu perfil! Ah, você pode
+                    editar essas informações depois.
+                </Text>
 
                 <MultiStep size={4} currentStep={1} />
             </Header>
 
-            <Form as='form' onSubmit={handleSubmit(handleRegister)}>
+            <Form as="form" onSubmit={handleSubmit(handleRegister)}>
                 <label>
                     <Text>Nome de usuário</Text>
                     {/* @ts-ignore */}
-                    <TextInput prefix='ignite.com/' placeholder='seu-usuario' {...register('username')} />
-                    {errors.username && <FormError size='sm'>{errors.username.message}</FormError>}
+                    <TextInput
+                        prefix="ignite.com/"
+                        placeholder="seu-usuario"
+                        {...register("username")}
+                    />
+                    {errors.username && (
+                        <FormError size="sm">{errors.username.message}</FormError>
+                    )}
                 </label>
 
                 <label>
                     <Text>Nome completo</Text>
                     {/* @ts-ignore */}
-                    <TextInput placeholder='Seu nome' {...register('name')} />
-                    {errors.name && <FormError size='sm'>{errors.name.message}</FormError>}
+                    <TextInput placeholder="Seu nome" {...register("name")} />
+                    {errors.name && (
+                        <FormError size="sm">{errors.name.message}</FormError>
+                    )}
                 </label>
 
-                <Button type='submit' disabled={isSubmitting}>Próximo passo <ArrowRight /></Button>
+                <Button type="submit" disabled={isSubmitting}>
+                    Próximo passo <ArrowRight />
+                </Button>
             </Form>
         </Container>
     )
