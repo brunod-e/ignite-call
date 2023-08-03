ca66eec7f8e824eea05b4f1d3829aad57e290cd9
Bruno <brunodaniel.elias@gmail.com>
2023-08-02 22:02:16 -0300

    finish register section

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -16,6 +16,9 @@ export default function ConnectCalendar() {
   async function handleConnectCalendar() {
     await signIn("google")
   }
+  async function handleNavigateToNextStep() {
+    await router.push("/register/time-intervals")
+  }
 
   return (
     <Container>
@@ -56,7 +59,11 @@ export default function ConnectCalendar() {
           </AuthError>
         )}
 
-        <Button type="submit" disabled={!isSignedId}>
+        <Button
+          onClick={handleNavigateToNextStep}
+          type="submit"
+          disabled={!isSignedId}
+        >
           Próximo passo
           <ArrowRight />
         </Button>
